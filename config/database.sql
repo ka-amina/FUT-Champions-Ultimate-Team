a5d69b016f9fec2c73862c8cdb3a27e8f44c575e
ka-amina <aminakara400@gmail.com>
2024-12-16 16:52:52 +0100

    CHAMPIONS-2: add dashboard

--- config/database.sql
+++ config/database.sql
@@ -1,34 +1,30 @@
 create database football;
 use football;
-create table club (
-club_id varchar(10) not null  unique primary key,
+create table CLUBS (
+club_id INT not null  unique primary key AUTO_INCREMENT,
 club_name varchar(20) ,
 club_logo varchar(20)
 );
 create table nationality (
-nationality_id varchar(10) unique primary key not null,
+nationality_id INT unique primary key not null AUTO_INCREMENT,
 country_name varchar(10) ,
 flag_image varchar(255)
 );
-create table player_position(
-position_id varchar(10) not null primary key,
-position_name enum('GK','ST','RW','LW','CM','LB','CB','RB'),
-position_category varchar(50)
-);
-create table players(
-player_id varchar(20) not null primary key,
+create table PLAYERS(
+player_id INT  not null primary key AUTO_INCREMENT,
 name varchar(20) ,
 photo varchar(255),
-club_id varchar(10),
+club_id INT ,
 nationality_id varchar(10),
-position_id varchar(10),
+position_name enum('GK','ST','RW','LW','CM','LB','CB','RB'),
 rating int not null,
+is_deleted tinyint(1),
 foreign key (club_id) references club(club_id),
 foreign key (nationality_id) references nationality(nationality_id),
 foreign key (position_id) references player_position(position_id)
 );
-create table outfield_player(
-player_id varchar(10) not null primary key,
+create table OUTFIELD_PLAYERS (
+player_id INT not null primary key,
 pace int not null,
 shooting int not null,
 passing int not null,
@@ -37,8 +33,8 @@ defending int not null,
 physical int not null,
 foreign key (player_id) references players (player_id)
 );
-create table goalkeepers(
-player_id varchar(10) not null primary key,
+create table GOALKEEPERS (
+player_id INT not null primary key,
 diving int not null,
 handling int not null,
 kicking int not null,

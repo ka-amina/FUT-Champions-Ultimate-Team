d7c79695ace917481e9899aca3b87a30a1f0043b
ka-amina <aminakara400@gmail.com>
2024-12-20 16:17:23 +0100

    add fomctionality of display some  players on index.php

--- config/database.sql
+++ config/database.sql
@@ -6,7 +6,7 @@ club_name varchar(20) ,
 club_logo varchar(255)
 );
 create table NATIONALITIES (
-nationality_id varchar(3) unique primary key not null AUTO_INCREMENT,
+nationality_id int unique primary key not null AUTO_INCREMENT,
 country_name varchar(10) ,
 flag_image varchar(255)
 );
@@ -46,22 +46,9 @@ foreign key (player_id) references players (player_id)
 
 -- insert data on tables
 
-INSERT INTO CLUBS (club_name, club_logo) 
-VALUES ('Inter Miami', 'https://cdn.sofifa.net/meta/team/239235/120.png'),
-       ('Al Nassr', 'https://cdn.sofifa.net/meta/team/2506/120.png'),
-       ('Manchester City', 'https://cdn.sofifa.net/players/239/085/25_120.png');
 INSERT INTO NATIONALITIES (country_name, flag_image)
 VALUES ('Argentina', 'https://cdn.sofifa.net/flags/ar.png'),
        ('Portugal', 'https://cdn.sofifa.net/flags/pt.png'),
        ('Belgium', 'https://cdn.sofifa.net/flags/be.png');
-INSERT INTO PLAYERS (name, photo, club_id, nationality_id, position_name, rating, is_deleted)
-VALUES ('David De Gea', 'degea_photo.png', 1, 1, 'GK', 85, 0),
-       ('Sergio Ramos', 'ramos_photo.png', 2, 2, 'CB', 87, 0),
-       ('Lionel Messi', 'messi_photo.png', 3, 3, 'RW', 95, 0); 
-INSERT INTO OUTFIELD_PLAYERS 
-VALUES (2, 80, 85, 90, 87, 75, 88),  
-       (3, 95, 92, 88, 96, 65, 82); 
-INSERT INTO GOALKEEPERS (player_id, diving, handling, kicking, refelexes, speed, positioning)
-VALUES (1, 90, 85, 80, 87, 70, 85);  
 
 

b5abd82d5ad06f47d85c6e856d5e7435e61f2788
ka-amina <aminakara400@gmail.com>
2024-12-21 13:18:23 +0100

    CHAMPIONS-41: ajouter les CM ET RB joeurs dans le terrain et aussi dans le changement

--- config/connection.php
+++ config/connection.php
@@ -197,6 +197,137 @@ join nationalities
 where position_name= 'LB'
 and is_deleted = 0
 ";
+$get_3cm_players= "SELECT 
+name,
+photo,
+club_logo,
+flag_image,
+position_name,
+rating,
+    pace,
+    shooting,
+    passing,
+    driblling,
+    defending,
+    physical,
+   diving,
+   handling,
+   kicking,
+   refelexes,
+   speed,
+    positioning 
+from players
+LEFT JOIN OUTFIELD_PLAYERS
+    ON players.player_id = OUTFIELD_PLAYERS.player_id
+LEFT JOIN GOALKEEPERS
+    ON players.player_id = GOALKEEPERS.player_id
+join clubs 
+     on clubs.club_id= players.club_id
+join nationalities 
+     on nationalities.nationality_id= players.nationality_id
+where position_name= 'CM'
+and is_deleted = 0
+limit 3
+";
+
+$get_cmchangemet_players="SELECT
+name,
+photo,
+club_logo,
+flag_image,
+position_name,
+rating,
+    pace,
+    shooting,
+    passing,
+    driblling,
+    defending,
+    physical,
+   diving,
+   handling,
+   kicking,
+   refelexes,
+   speed,
+    positioning 
+from players
+LEFT JOIN OUTFIELD_PLAYERS
+    ON players.player_id = OUTFIELD_PLAYERS.player_id
+LEFT JOIN GOALKEEPERS
+    ON players.player_id = GOALKEEPERS.player_id
+join clubs 
+     on clubs.club_id= players.club_id
+join nationalities 
+     on nationalities.nationality_id= players.nationality_id
+where position_name= 'CM'
+and is_deleted = 0
+limit 10 offset 3"; 
+
+$get_2cb_players= "SELECT 
+name,
+photo,
+club_logo,
+flag_image,
+position_name,
+rating,
+    pace,
+    shooting,
+    passing,
+    driblling,
+    defending,
+    physical,
+   diving,
+   handling,
+   kicking,
+   refelexes,
+   speed,
+    positioning 
+from players
+LEFT JOIN OUTFIELD_PLAYERS
+    ON players.player_id = OUTFIELD_PLAYERS.player_id
+LEFT JOIN GOALKEEPERS
+    ON players.player_id = GOALKEEPERS.player_id
+join clubs 
+     on clubs.club_id= players.club_id
+join nationalities 
+     on nationalities.nationality_id= players.nationality_id
+where position_name= 'CB'
+and is_deleted = 0
+limit 2
+";
+
+$get_cbchangemet_players="SELECT
+name,
+photo,
+club_logo,
+flag_image,
+position_name,
+rating,
+    pace,
+    shooting,
+    passing,
+    driblling,
+    defending,
+    physical,
+   diving,
+   handling,
+   kicking,
+   refelexes,
+   speed,
+    positioning 
+from players
+LEFT JOIN OUTFIELD_PLAYERS
+    ON players.player_id = OUTFIELD_PLAYERS.player_id
+LEFT JOIN GOALKEEPERS
+    ON players.player_id = GOALKEEPERS.player_id
+join clubs 
+     on clubs.club_id= players.club_id
+join nationalities 
+     on nationalities.nationality_id= players.nationality_id
+where position_name= 'CB'
+and is_deleted = 0
+limit 10 offset 2"; 
+
+$get_deleted_players= "SELECT * FROM players where is_deleted=1";
 $get_contries="SELECT nationality_id ,country_name FROM NATIONALITIES";
 
 $get_clubs = "SELECT club_id, club_name,club_logo FROM CLUBS";

0c53969ff23cb6a8e2569860dd3391c701ad004e
ka-amina <aminakara400@gmail.com>
2024-12-22 23:25:29 +0100

    ajouter la fonctionalité de changer la language en francais et par defaut en englais

--- views/dashboard/clubs.php
+++ views/dashboard/clubs.php
@@ -1,5 +1,6 @@
 <?php
 include '../../config/connection.php';
+include '../../assets/lang/lang.php';
 if (!$connection) {
     die("Connection failed: " . mysqli_connect_error());
 }
@@ -68,67 +69,31 @@ if (isset($_POST['addclub'])) {
                 <li>
                     <a href="dashboard.php" aria-label="dashboard"
                         class="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group">
-                        <svg class="-ml-1 h-6 w-6" viewBox="0 0 24 24" fill="none">
-                            <path
-                                d="M6 8a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2V8ZM6 15a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2v-1Z"
-                                class="fill-current text-cyan-400 dark:fill-slate-600"></path>
-                            <path d="M13 8a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2V8Z"
-                                class="fill-current text-cyan-200 group-hover:text-cyan-300"></path>
-                            <path d="M13 15a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2v-1Z"
-                                class="fill-current group-hover:text-sky-300"></path>
-                        </svg>
-                        <span class="-mr-1 font-medium">Dashboard</span>
+                        <span class="-mr-1 font-medium"><?= $language['Dashboard']?></span>
                     </a>
                 </li>
                 <li>
                     <a href="players.php"
-                        class="relative px-4 py-3 flex items-center space-x-4 rounded-xl text-white bg-gradient-to-r from-sky-600 to-cyan-400">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                            <path class="fill-current text-gray-300 group-hover:text-cyan-300" fill-rule="evenodd"
-                                d="M2 6a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1H8a3 3 0 00-3 3v1.5a1.5 1.5 0 01-3 0V6z"
-                                clip-rule="evenodd" />
-                            <path class="fill-current text-gray-600 group-hover:text-cyan-600"
-                                d="M6 12a2 2 0 012-2h8a2 2 0 012 2v2a2 2 0 01-2 2H2h2a2 2 0 002-2v-2z" />
-                        </svg>
-                        <span class="group-hover:text-gray-700">Players</span>
+                        class="relative px-4 py-3 flex items-center space-x-4 rounded-xl text-gray-600 group ">
+                        <span class="group-hover:text-gray-700"><?= $language['players']?></span>
                     </a>
                 </li>
                 <li>
                     <a href="../home/index.php"
                         class="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                            <path class="fill-current text-gray-600 group-hover:text-cyan-600" fill-rule="evenodd"
-                                d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z"
-                                clip-rule="evenodd" />
-                            <path class="fill-current text-gray-300 group-hover:text-cyan-300"
-                                d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z" />
-                        </svg>
-                        <span class="group-hover:text-gray-700">Views formation </span>
+                        <span class="group-hover:text-gray-700"><?= $language['formation']?></span>
                     </a>
                 </li>
                 <li>
                     <a href="DeletedPlayers.php"
                         class="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                            <path class="fill-current text-gray-600 group-hover:text-cyan-600"
-                                d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z" />
-                            <path class="fill-current text-gray-300 group-hover:text-cyan-300"
-                                d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z" />
-                        </svg>
-                        <span class="group-hover:text-gray-700">Deleted players</span>
+                        <span class="group-hover:text-gray-700"><?= $language['Dplayers']?></span>
                     </a>
                 </li>
                 <li>
-                    <a href="nationalities.php"
-                        class="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                            <path class="fill-current text-gray-300 group-hover:text-cyan-300"
-                                d="M4 4a2 2 0 00-2 2v1h16V6a2 2 0 00-2-2H4z" />
-                            <path class="fill-current text-gray-600 group-hover:text-cyan-600" fill-rule="evenodd"
-                                d="M18 9H2v5a2 2 0 002 2h12a2 2 0 002-2V9zM4 13a1 1 0 011-1h1a1 1 0 110 2H5a1 1 0 01-1-1zm5-1a1 1 0 100 2h1a1 1 0 100-2H9z"
-                                clip-rule="evenodd" />
-                        </svg>
-                        <span class="group-hover:text-gray-700">Countries</span>
+                    <a href="clubs.php"
+                        class="px-4 py-3 flex items-center space-x-4 rounded-md text-white bg-gradient-to-r from-sky-600 to-cyan-400">
+                        <span class="group-hover:text-gray-700"><?= $language['Clubs']?></span>
                     </a>
                 </li>
             </ul>
@@ -138,7 +103,7 @@ if (isset($_POST['addclub'])) {
     <div class="ml-auto mb-6 lg:w-[75%] xl:w-[80%] 2xl:w-[85%]">
         <div class="sticky z-10 top-0 h-16 border-b bg-white lg:py-2.5">
             <div class="px-6 flex items-center justify-between space-x-4 2xl:container">
-                <h5 hidden class="text-2xl text-gray-600 font-medium lg:block">Players</h5>
+                <h5 hidden class="text-2xl text-gray-600 font-medium lg:block"><?= $language['Clubs']?></h5>
                 <button class="w-12 h-16 -mr-2 border-r lg:hidden">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 my-auto" fill="none" viewBox="0 0 24 24"
                         stroke="currentColor">
@@ -147,46 +112,13 @@ if (isset($_POST['addclub'])) {
                     </svg>
                 </button>
                 <div class="flex space-x-4">
-                    <!--search bar -->
-                    <div hidden class="md:block">
-                        <div class="relative flex items-center text-gray-400 focus-within:text-cyan-400">
-                            <span class="absolute left-4 h-6 flex items-center pr-3 border-r border-gray-300">
-                                <svg xmlns="http://ww50w3.org/2000/svg" class="w-4 fill-current"
-                                    viewBox="0 0 35.997 36.004">
-                                    <path id="Icon_awesome-search" data-name="search"
-                                        d="M35.508,31.127l-7.01-7.01a1.686,1.686,0,0,0-1.2-.492H26.156a14.618,14.618,0,1,0-2.531,2.531V27.3a1.686,1.686,0,0,0,.492,1.2l7.01,7.01a1.681,1.681,0,0,0,2.384,0l1.99-1.99a1.7,1.7,0,0,0,.007-2.391Zm-20.883-7.5a9,9,0,1,1,9-9A8.995,8.995,0,0,1,14.625,23.625Z">
-                                    </path>
-                                </svg>
-                            </span>
-                            <input type="search" name="leadingIcon" id="leadingIcon" placeholder="Search here"
-                                class="w-full pl-14 pr-4 py-2.5 rounded-xl text-sm text-gray-600 outline-none border border-gray-300 focus:border-cyan-300 transition">
-                        </div>
-                    </div>
-                    <!--/search bar -->
-                    <button aria-label="search"
-                        class="w-10 h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200 md:hidden">
-                        <svg xmlns="http://ww50w3.org/2000/svg" class="w-4 mx-auto fill-current text-gray-600"
-                            viewBox="0 0 35.997 36.004">
-                            <path id="Icon_awesome-search" data-name="search"
-                                d="M35.508,31.127l-7.01-7.01a1.686,1.686,0,0,0-1.2-.492H26.156a14.618,14.618,0,1,0-2.531,2.531V27.3a1.686,1.686,0,0,0,.492,1.2l7.01,7.01a1.681,1.681,0,0,0,2.384,0l1.99-1.99a1.7,1.7,0,0,0,.007-2.391Zm-20.883-7.5a9,9,0,1,1,9-9A8.995,8.995,0,0,1,14.625,23.625Z">
-                            </path>
-                        </svg>
-                    </button>
-                    <button aria-label="chat"
-                        class="w-10 h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 m-auto text-gray-600" fill="none"
-                            viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
-                        </svg>
+                    <button aria-label="notification"
+                        class="w-24 h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200">
+                        <a href="?lang=fr"><?= $language['French']?></a>
                     </button>
                     <button aria-label="notification"
-                        class="w-10 h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 m-auto text-gray-600" viewBox="0 0 20 20"
-                            fill="currentColor">
-                            <path
-                                d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z" />
-                        </svg>
+                        class="w-24 h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200">
+                        <a href="?lang=en"><?= $language['English']?></a>
                     </button>
                 </div>
             </div>
@@ -194,11 +126,11 @@ if (isset($_POST['addclub'])) {
 
         <div class="mt-8 bg-white p-4 shadow rounded-lg clubs-table">
             <div class="flex justify-between items-center">
-                <h2 class="text-gray-500 text-lg font-semibold pb-4">Clubs table</h2>
+                <h2 class="text-gray-500 text-lg font-semibold pb-4"><?= $language['tclubs']?></h2>
 
                 <button class="bg-cyan-500 hover:bg-cyan-600 text-white font-semibold py-2 px-4 rounded "
                     id="show-form-country">
-                    Add Club
+                    <?= $language['addclubs']?>
                 </button>
             </div>
 
@@ -209,19 +141,19 @@ if (isset($_POST['addclub'])) {
                     <tr class="text-sm leading-normal">
                         <th
                             class="w-1/4 py-2 px-4 bg-grey-lightest font-bold uppercase text-sm text-grey-light border-b border-grey-light">
-                            id
+                            <?= $language['id']?>
                         </th>
                         <th
                             class="w-1/4 py-2 px-4 bg-grey-lightest font-bold uppercase text-sm text-grey-light border-b border-grey-light">
-                            club name
+                            <?= $language['clubname']?>
                         </th>
                         <th
                             class=" w-1/4 py-2 px-4 bg-grey-lightest font-bold uppercase text-sm text-grey-light border-b border-grey-light">
-                            logo
+                            <?= $language['logo']?>
                         </th>
                         <th
                             class=" w-1/4 py-2 px-4 bg-grey-lightest font-bold uppercase text-sm text-grey-light border-b border-grey-light">
-                            actions
+                            <?= $language['actions']?>
                         </th>
                     </tr>
                 </thead>
@@ -267,26 +199,26 @@ if (isset($_POST['addclub'])) {
                         enctype="multipart/form-data">
                         <div
                             class="p-4 border-b-2 w-full text-center font-bold border-b-black flex justify-between items-center">
-                            <span>Add Club</span>
+                            <span><?= $language['addclubs']?></span>
                             <span class="close-form cursor-pointer"><img src="../../assets/icons/close.svg" alt=""
                                     width="24" height="24" /></span>
                         </div>
 
                         <div class="flex flex-col ">
-                            <label for="name" class="name my-2">Club name</label>
+                            <label for="name" class="name my-2"><?= $language['clubname']?></label>
                             <input id="player-name" name="clubname" type="text" placeholder="name"
                                 class="p-2 w-52 rounded border border-gray-500" />
                         </div>
                         <p id="player-name-error" class="w-80 hidden text-error"></p>
 
                         <div class="flex flex-col w-52">
-                            <label for="photo" class="mb-2">image</label>
+                            <label for="photo" class="mb-2"><?= $language['photo']?></label>
                             <img src="" alt="Profile Image" id="showImg" class="w-40 h-40 hidden mb-6" />
                             <input id="profile-img" name="clubImg" type="file" accept=".jpg, .png, .jpeg, .webp"
                                 class="hidden" />
                             <label for="profile-img"
                                 class="cursor-pointer bg-primary text-white py-1 px-2 rounded text-center">
-                                Select Image
+                                <?= $language['selectimage']?>
                             </label>
                         </div>
 
@@ -295,7 +227,7 @@ if (isset($_POST['addclub'])) {
                         <div id="change" class="mt-8">
                             <button id="addNewPlayer" type="submit" name="addclub"
                                 class="bg-primary hover:bg-primary-hover text-white p-2 w-80 rounded mb-6">
-                                Add Club
+                                <?= $language['add-club-btn']?>
                             </button>
                         </div>
                     </form>
